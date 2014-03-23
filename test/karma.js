4cdba6eab04c9f47e148348e89b7eb6ab9e953ca
tomchentw <developer@tomchentw.com>
2014-03-23 19:05:09 +0800

    fix(test/karma): coverage on application.ls

--- test/karma.js
+++ test/karma.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'lib/javascripts/tc-spa-boilerplate.ls': ['coverage']
+      'client/javascripts/application.ls': ['coverage']
     },
 
     // web server port
