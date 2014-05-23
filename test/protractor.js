cad21f20f6e68078ed27b139a9b296cf6a8751d8
tomchentw <developer@tomchentw.com>
2014-05-24 00:45:31 +0800

    chore(test/protractor): update config to test on travis

--- test/protractor.js
+++ test/protractor.js
@@ -1,52 +1,7 @@
 require('LiveScript');
 var config = require('../config');
 // A reference configuration file.
-var baseConfig = exports.config = {
-  // ----- How to setup Selenium -----
-  //
-  // There are three ways to specify how to use Selenium. Specify one of the
-  // following:
-  //
-  // 1. seleniumServerJar - to start Selenium Standalone locally.
-  // 2. seleniumAddress - to connect to a Selenium server which is already
-  //    running.
-  // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
-  //
-  // If the chromeOnly option is specified, no Selenium server will be started,
-  // and chromeDriver will be used directly (from the location specified in
-  // chromeDriver)
-
-  // The location of the selenium standalone server .jar file, relative
-  // to the location of this config. If no other method of starting selenium
-  // is found, this will default to
-  // node_modules/protractor/selenium/selenium-server...
-  seleniumServerJar: null,
-  // The port to start the selenium server on, or null if the server should
-  // find its own unused port.
-  seleniumPort: null,
-  // Chromedriver location is used to help the selenium standalone server
-  // find chromedriver. This will be passed to the selenium jar as
-  // the system property webdriver.chrome.driver. If null, selenium will
-  // attempt to find chromedriver using PATH.
-  chromeDriver: null,
-  // If true, only chromedriver will be started, not a standalone selenium.
-  // Tests for browsers other than chrome will not run.
-  chromeOnly: false,
-  // Additional command line options to pass to selenium. For example,
-  // if you need to change the browser timeout, use
-  // seleniumArgs: ['-browserTimeout=60'],
-  seleniumArgs: [],
-
-  // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
-  // The tests will be run remotely using SauceLabs.
-  sauceUser: null,
-  sauceKey: null,
-
-  // The address of a running selenium server. If specified, Protractor will
-  // connect to an already running instance of selenium. This usually looks like
-  // seleniumAddress: 'http://localhost:4444/wd/hub'
-  seleniumAddress: null,
-
+exports.config = {
   // The timeout for each script run on the browser. This should be longer
   // than the maximum time your application needs to stabilize between tasks.
   allScriptsTimeout: 11000,
@@ -65,7 +20,8 @@ var baseConfig = exports.config = {
   // and
   // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': process.env.TRAVIS ? 'firefox' : 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
   },
 
   // ----- More information for your tests ----
@@ -74,32 +30,12 @@ var baseConfig = exports.config = {
   // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:' + config.port.server,
 
-  // Selector for the element housing the angular app - this defaults to
-  // body, but is necessary if ng-app is on a descendant of <body>  
-  rootElement: 'body',
-
-  // A callback function called once protractor is ready and available, and
-  // before the specs are executed
-  // You can specify a file containing code to run by setting onPrepare to
-  // the filename string.
-  onPrepare: function() {
-    // At this point, global 'protractor' object will be set up, and jasmine
-    // will be available. For example, you can add a Jasmine reporter with:
-    //     jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter(
-    //         'outputdir/', true, true));
-  },
-
   // The params object will be passed directly to the protractor instance,
   // and can be accessed from your test. It is an arbitrary object and can
   // contain anything you may need in your test.
   // This can be changed via the command line as:
   //   --params.login.user 'Joe'
-  params: {
-    login: {
-      user: 'Jane',
-      password: '1234'
-    }
-  },
+  params: null,
   
   // ----- Options to be passed to minijasminenode -----
   //
@@ -117,17 +53,3 @@ var baseConfig = exports.config = {
     defaultTimeoutInterval: 30000
   }
 };
-
-if (process.env.TRAVIS) {
-  baseConfig.sauceUser = process.env.SAUCE_USERNAME;
-  baseConfig.sauceKey = process.env.SAUCE_ACCESS_KEY;
-
-  baseConfig.capabilities['tunnel-identifier'] = process.env.TRAVIS_JOB_NUMBER;
-} else {// local
-  baseConfig.seleniumServerJar = '../node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar';
-  baseConfig.chromeDriver = '../node_modules/protractor/selenium/chromedriver';
-}
-
-
-
-
